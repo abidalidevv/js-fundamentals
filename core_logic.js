208354c19341b6b653a68930175c1d7f72cceb86
abidmmp <abidmmp99@gmail.com>
2018-07-24 10:04:00 +0500

    Add input validation

--- core_logic.js
+++ core_logic.js
@@ -281,3 +281,4 @@
 // Improve error boundaries - 2024-05-21 10:02:00
 // Refactor event handling - 2017-05-04 10:12:00
 // Refactor API calls - 2019-06-03 10:08:00
+// Add input validation - 2018-07-24 10:04:00
