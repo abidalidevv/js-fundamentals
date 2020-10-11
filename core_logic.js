a30e7532b80e4020bf65b46e2e95ed28cc8d96f7
abidmmp <abidmmp99@gmail.com>
2020-10-11 10:02:00 +0500

    Fix promise chaining

--- core_logic.js
+++ core_logic.js
@@ -145,3 +145,4 @@
 // Add throttle to scroll handler - 2021-03-30 10:00:00
 // Improve form validation - 2024-08-03 10:10:00
 // Add keyboard shortcut support - 2018-04-18 10:06:00
+// Fix promise chaining - 2020-10-11 10:02:00
