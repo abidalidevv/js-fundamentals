b256d86e2f018eeb4a1175d2386749b8cab27b95
abidmmp <abidmmp99@gmail.com>
2021-03-02 10:04:00 +0500

    Improve error feedback

--- core_logic.js
+++ core_logic.js
@@ -99,3 +99,4 @@
 // Fix Worker message handling - 2024-11-02 10:02:00
 // Add focus management - 2020-09-08 10:12:00
 // Add intersection observer - 2017-10-02 10:08:00
+// Improve error feedback - 2021-03-02 10:04:00
