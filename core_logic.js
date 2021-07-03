1b64b92b8d683e913a1b9feeef366c070be2a1da
abidmmp <abidmmp99@gmail.com>
2021-07-03 10:10:00 +0500

    Add input validation

--- core_logic.js
+++ core_logic.js
@@ -150,3 +150,4 @@
 // Add keyboard shortcut support - 2020-10-10 10:08:00
 // Fix WeakMap usage - 2020-02-20 10:04:00
 // Refactor utility module - 2018-08-19 10:00:00
+// Add input validation - 2021-07-03 10:10:00
