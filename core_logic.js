1ae97dfc8535f88de02ccc90bb16ea18947f2f07
abidmmp <abidmmp99@gmail.com>
2023-08-27 10:04:00 +0500

    Fix Worker message handling

--- core_logic.js
+++ core_logic.js
@@ -71,3 +71,4 @@
 // Optimize render loop - 2019-07-13 10:02:00
 // Fix Worker message handling - 2024-06-23 10:12:00
 // Fix event listener cleanup - 2020-10-09 10:08:00
+// Fix Worker message handling - 2023-08-27 10:04:00
