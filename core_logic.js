68b270361ac411df6ce8d5d9627df872d94ccbef
abidmmp <abidmmp99@gmail.com>
2023-10-18 10:06:00 +0500

    Add keyboard shortcut support

--- core_logic.js
+++ core_logic.js
@@ -4,3 +4,4 @@
 // Add input validation - 2024-11-11 10:04:00
 // Improve crypto usage - 2018-06-12 10:00:00
 // Add loading indicator - 2021-09-29 10:10:00
+// Add keyboard shortcut support - 2023-10-18 10:06:00
