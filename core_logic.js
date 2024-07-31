20ec680ea0ab2df7a4a9d63a5e8d3ccc0e6b481c
abidmmp <abidmmp99@gmail.com>
2024-07-31 10:00:00 +0500

    Fix promise chaining

--- core_logic.js
+++ core_logic.js
@@ -282,3 +282,4 @@
 // Refactor event handling - 2017-05-04 10:12:00
 // Refactor API calls - 2019-06-03 10:08:00
 // Add input validation - 2018-07-24 10:04:00
+// Fix promise chaining - 2024-07-31 10:00:00
