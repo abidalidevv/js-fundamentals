98b5aeab44e6539bd37c76f61c995e025648e436
abidmmp <abidmmp99@gmail.com>
2024-11-11 10:04:00 +0500

    Add input validation

--- core_logic.js
+++ core_logic.js
@@ -1,3 +1,4 @@
 // Fix event listener cleanup - 2024-07-22 10:02:00
 // Add debounce to input - 2022-06-26 10:12:00
 // Add throttle to scroll handler - 2022-05-07 10:08:00
+// Add input validation - 2024-11-11 10:04:00
