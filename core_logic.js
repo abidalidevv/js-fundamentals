30a8d1d6d5f3b63efb98148512ca8e22db6a5d26
Abid Ali <abidmmp99@gmail.com>
2025-03-27 11:41:39 +0000

    Fix CORS handling

--- core_logic.js
+++ core_logic.js
@@ -333,3 +333,5 @@ const pick = (obj, keys) => Object.fromEntries(keys.filter(k=>k in obj).map(k=>[
 const unique = arr => [...new Set(arr)];
 
 const range = (s, e, step=1) => { const r=[]; for(let i=s;i<e;i+=step) r.push(i); return r; };
+
+const omit = (obj, keys) => Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
