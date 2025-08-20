2ba15ea2a3d798afec6ae17972e8509016556a1d
Abid Ali <abidmmp99@gmail.com>
2025-08-20 20:31:41 +0000

    Fix edge case in handler

--- core_logic.js
+++ core_logic.js
@@ -327,3 +327,5 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 const deepClone = obj => JSON.parse(JSON.stringify(obj));
+
+const pick = (obj, keys) => Object.fromEntries(keys.filter(k=>k in obj).map(k=>[k,obj[k]]));
